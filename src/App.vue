a9b6e11174dff6c6ac2f9fb85559bbbc26b7a28f
afnanfauzi <afnanfauzihidayat@gmail.com>
2021-12-01 19:00:44 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 d-flex">
+        <div class="col-6">
+          <h2>Monthly Sales Line & Bar Chart</h2>
+          <monthly-sales-chart></monthly-sales-chart>
+        </div>
+        <div class="col-6">
+          <h2>Monthly Sales Doughnut & Pie Chart</h2>
+          <DoughnutAndPieChart></DoughnutAndPieChart>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MonthlySalesChart from './components/MonthlySalesChart';
+import DoughnutAndPieChart from './components/DoughnutAndPieChart';
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    MonthlySalesChart,
+    DoughnutAndPieChart
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/DoughnutAndPieChart.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div class="field is-grouped">
+      <div class="control">
+        <label class="checkbox">
+          2018 Sales
+        </label>
+      </div>
+    </div>
+
+    <doughnut-chart
+      :width="50"
+      :height="30"
+      :labels="['Jan', 'Feb', 'Mar']"
+      :datasets="displayedDatasets"
+    ></doughnut-chart>
+    <pie-chart
+      :width="50"
+      :height="30"
+      :labels="['Jan', 'Feb', 'Mar']"
+      :datasets="displayedDatasets"
+    ></pie-chart>
+    
+  </div>
+</template>
+
+<script>
+import DoughnutChart from './DoughnutChart';
+import PieChart from './PieChart';
+
+
+const datasets = {
+  2018: {
+    label: '2018 Sales',
+    hoverOffset: 4,
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    data: [300, 50, 100]
+  }
+};
+
+// const options = {
+//   scales: {
+//     yAxes: [{
+//       ticks: {
+//         beginAtZero: true,
+//         callback: value => numeral(value).format('$0,0')
+//       }
+//     }]
+//   },
+//   tooltips: {
+//     mode: 'index',
+//     callbacks: {
+//       label(tooltipItem, data) {
+//         const label = data.datasets[tooltipItem.datasetIndex].label;
+//         const value = numeral(tooltipItem.yLabel).format('$0,0');
+
+//         return `${label}: ${value}`;
+//       }
+//     }
+//   }
+// };
+
+export default {
+  name: 'doughnut-pie',
+  datasets,
+  components: {
+    DoughnutChart,
+    PieChart
+  },
+  data() {
+    return {
+      selectedYears: [2018]
+    };
+  },
+  computed: {
+    displayedDatasets() {
+      return this.selectedYears.map(year => datasets[year]);
+    }
+  }
+}
+</script>
